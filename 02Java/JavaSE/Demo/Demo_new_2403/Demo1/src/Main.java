d3ca75c99d4b26c90b8e200add9730a6b755c3be
小火锅好好吃 <58215622+TheonlyCR7@users.noreply.github.com>
2024-03-30 10:55:13 +0800

    update

---
+++ 02Java/JavaSE/Demo/Demo_new_2403/Demo1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "hello";
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equalsIgnoreCase(str2));
+    }
+}
---
+++ 02Java/JavaSE/Demo/Demo_new_2403/Demo1/src/test.java
@@ -0,0 +1,26 @@
+
+class funtion {
+    public static void main (String[] args){
+        dog one = new dog();
+        one.setsize(70);
+        //修改one对象的属性 size
+//        one.size = 20;
+        System.out.println(one.getsize());
+    }
+}
+//其他类要在主类后面
+class dog {
+    //实例变量在类中定义
+    //实例变量不赋值时，也是有值的
+    //使用private  将变量私有化，安全
+    private int size;
+    public int getsize() {
+        //局部变量在函数中定义，不赋值没有值，必须初始化
+        String bad;
+        return size;
+    }
+    //主函数的变量无法共享
+    public void setsize(int si) {
+        size = si + si;
+    }
+}
